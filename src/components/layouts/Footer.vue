de22127409821bc1990c13502e2f085932a72045
Maxwell Adapoe <maxwelladapoe@gmail.com>
2020-12-18 05:49:57 +0000

    added footer icons

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -5,9 +5,26 @@
             <div class="container">
                 <div class="columns">
                     <div class="column is-3">
-
+                        <p><span class="has-text-weight-bold ">maxwell adapoe</span>
+                        </p>
+                        <p>Fullstack Development, Graphic Design & UI Design</p>
                     </div>
-                    <div class="is-9">
+                    <div class="is-6 is-offset-3">
+
+                        <div class="social-wrap">
+                            <div class="social-icon">
+                               <p> <b-icon icon="facebook"/></p>
+                            </div>
+                            <div class="social-icon">
+                                <p> <b-icon icon="twitter"/></p>
+                            </div>
+                            <div class="social-icon">
+                                <p> <b-icon icon="linkedin"/></p>
+                            </div>
+                            <div class="social-icon">
+                                <p> <b-icon icon="github"/> </p>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
